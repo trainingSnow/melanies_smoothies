3802e105a7b58c0e034582a7b6112972f97ba250
trainingSnow <cjschupp15@yahoo.com>
2024-08-27 12:28:13 -0500

    Update streamlit_app.py
    moved last if statment back 4 spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,9 @@ if ingredients_list:
                 #st.write(my_insert_stmt)
                 #st.stop()
                 time_to_insert = st.button('Submit Order')
-        if time_to_insert:
-                session.sql(my_insert_stmt).collect()
-                st.success('Your Smoothie is ordered,'' '+name_on_order+'''!''', icon="✅")
+if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered,'' '+name_on_order+'''!''', icon="✅")
 
 
 
