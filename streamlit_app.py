cdd6d1eefa4c01856eb1dad6517ac3203c017806
trainingSnow <cjschupp15@yahoo.com>
2024-08-27 10:28:52 -0500

    Update streamlit_app.py
    fixing indent for the if statment

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,18 +33,18 @@ if ingredients_list:
                 #st.text(fruityvice_response.json())
                 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
+                #st.write(ingredients_string)
     
 
                 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                             values ('"""+ ingredients_string +"""','"""+name_on_order+ """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered,'' '+name_on_order+'''!''', icon="✅")
+                #st.write(my_insert_stmt)
+                #st.stop()
+                time_to_insert = st.button('Submit Order')
+        if time_to_insert:
+                session.sql(my_insert_stmt).collect()
+                st.success('Your Smoothie is ordered,'' '+name_on_order+'''!''', icon="✅")
 
 
 
