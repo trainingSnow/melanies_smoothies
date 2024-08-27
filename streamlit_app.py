8f9742e0013384773d6ecdca86058990301ddc33
trainingSnow <cjschupp15@yahoo.com>
2024-08-27 14:13:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
                 #st.text(fruityvice_response.json())
                 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-                #st.write(ingredients_string)
+        #st.write(ingredients_string)
     
 
                 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
