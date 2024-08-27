1602b1ef3f7409538ebb4347a4e33554ff767297
trainingSnow <cjschupp15@yahoo.com>
2024-08-27 09:48:51 -0500

    Update streamlit_app.py
    adding .json() to end of line 47

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'' '+name_on_order+'''!''', icon="✅")
 
+#new section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
